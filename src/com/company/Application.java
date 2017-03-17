2fd957cc6d8d1819b5273257c7b36a730c258312
Veszner D <vekki64@gmail.com>
2017-03-17 11:44:24 +0100

    Szkeleton

--- src/com/company/Application.java
+++ src/com/company/Application.java
@@ -9,15 +9,11 @@ public class Application {
 
         //////////////////////////////////////////////////////////
         // init:
-        Thread t = new Thread();            // szál és game példányosítása
         Game game = new Game();
-        boolean gameIsOn = true;            // játék állapotát rögzítő bool (true, ha megy a játék)
         int counter = 0;                    // számláló (vonat generálásához) és a map
         int mapNumber = 1;                  // hányadik pályán járunk
         game.loadMap(mapNumber);            // első pálya betöltése
         //////////////////////////////////////////////////////////
-
-
 
 
 
@@ -52,13 +48,20 @@ public class Application {
                         // Perform "decrypt number" case.
                         break;
                     case 4:
+                        System.out.println("[Application].Main():");
+                        game.generateTrain();
                         break;
                     case 5:
+                        System.out.println("[Application].Main():");
+                        game.moveTrains();
                         break;
                     case 6:
+                        System.out.println("[Application].Main():");
+                        game.emptyCars();
                         break;
                     case 7:
                         System.out.println("[Application].Main():");
+                        game.crashDetection();
                         break;
                     case 8:
                         System.out.println("[Application].Main():");
@@ -75,60 +78,5 @@ public class Application {
 
 
         }
-
-    /*
-
-        //////////////////////////////////////////////////////////
-        // játék fő ciklusa:
-        // a példányosított játék függvényeit körönként, egy ciklusban hívjuk meg
-        while (gameIsOn) {
-
-            /////////////
-            // A megfelelő körökben vonatokat generálunk
-            if (counter == 0 || counter == 11)
-                game.generateTrain();
-
-
-            /////////////
-            // léptetést végrehajtó függvény
-            game.moveTrains();
-
-            game.emptyCars();
-
-            /////////////
-            // ütközéseket / játék végét detektáló függvény
-            // a true-val tér vissza, ha a játék valamilyen oknál fogva véget ér
-            if (game.crashDetection())
-                gameIsOn = false;
-
-
-            /////////////
-            // Minden kör végén a játék megnyerését vizsgáló függvény
-            // Ha minden vonat minden kocsija üres átugrunk a következő pályára
-            // Hogy az elején ne nyerjük meg rögtön, körszámlálót is ellenőrizzük
-            if (game.isWon() && counter > 5) {
-
-                // Ha az utolsó pályát nyertük meg, vége a játéknak
-                if ( game.getIsLastGame())
-                    gameIsOn = false;
-                // Amúgy csak a köv pályát töltjük be
-                else {
-                    game.deleteTrains();
-                    game.loadMap(2);
-                }
-            }
-
-
-            /////////////
-            // várakozás és a körszámláló növelése
-            t.sleep(1000);
-            counter++;
-
-        }
-        //////////////////////////////////////////////////////////
-
-*/
-
-
     }
 }
