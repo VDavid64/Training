dd7fee3d932d35520014a43153684cac9b503352
vidoree <sivodora@gmail.com>
2017-03-19 10:17:05 +0100

    javadoc added to Tunnel class

--- src/com/company/Tunnel.java
+++ src/com/company/Tunnel.java
@@ -1,66 +1,88 @@
 package com.company;
 
-
+/**
+ * Represents tunnel point.
+ * 
+ * @author i_did_iit team
+ *
+ */
 public class Tunnel extends Rail {
 
-    private boolean isActive;
+	/**
+	 * isActive whether tunnel point is active.
+	 */
+	private boolean isActive;
 
+	/**
+	 * Getter of isActive. Returns true whether tunnel point is active.
+	 * 
+	 * @return isActive
+	 */
+	public boolean isActive() {
+		return isActive;
+	}
 
-    public boolean isActive() {
-        return isActive;
-    }
+	/**
+	 * Setter of isActive. Set value of parameter to isActive attribute.
+	 * 
+	 * @param active
+	 *            True whether tunnel point is active.
+	 * @param seq
+	 *            Number of sequence. Important because of tabulators.
+	 */
+	public void setActive(boolean active, int seq) {
+		if (seq == 1) {
+			System.out.println("							-> [Tunnel].setActive(true)");
+			System.out.println("							<- [Tunnel].setActive(true)");
+		} else if (seq == 2) {
+			System.out.println("						-> [Tunnel].setActive(true)");
+			System.out.println("						<- [Tunnel].setActive(true)");
+		} else if (seq == 3) {
+			System.out.println("        -> [Tunnel].setActive(false)");
+			System.out.println("        <- [Tunnel].setActive(false)");
+		} else if (seq == 2) {
+			System.out.println("				-> [Tunnel].setActive(false)");
+			System.out.println("				<- [Tunnel].setActive(false)");
+		}
+		isActive = active;
+		return;
+	}
 
+	/**
+	 * Get the next rail. Returns a Rail object what is the next one.
+	 * 
+	 * @param Previous
+	 *            Rail object where train element was.
+	 * @param Train
+	 *            element which does move.
+	 * @return Next rail object.
+	 */
+	public Rail getNextRail(Rail previous, Train_Element t) {
+		if (previous == prevRail) {
+			// ha aktív a tunnel és a következő rail alatt megy alagút, és még
+			// nincs vonat az alagútban
+			if (isActive() && nextRail.isTunnelUnderRail && !Map.getIsTrainInTunnel()) {
+				t.setVisible(false);
+				Map.setIsTrainInTunnel(true);
+				return nextRail;
+			}
 
-    public void setActive(boolean active,int seq) {
-    	if (seq==1){
-    		System.out.println("							-> [Tunnel].setActive(true)");
-            System.out.println("							<- [Tunnel].setActive(true)");
-    	}
-    	else if (seq==2){
-    		System.out.println("						-> [Tunnel].setActive(true)");
-            System.out.println("						<- [Tunnel].setActive(true)");
-    	}
-    	else if (seq == 3){
-            System.out.println("        -> [Tunnel].setActive(false)");
-            System.out.println("        <- [Tunnel].setActive(false)");
-        }
-    	else if (seq==2){
-    		System.out.println("				-> [Tunnel].setActive(false)");
-            System.out.println("				<- [Tunnel].setActive(false)");
-    	}
-        return;
-    }
+			// ellenkező esetben haladunk tovább a felszínen
+			else
+				return nextRail;
+		}
 
+		else {
+			if (this.isActive && prevRail.isTunnelUnderRail && !Map.getIsTrainInTunnel()) {
+				t.setVisible(false);
+				Map.setIsTrainInTunnel(true);
+				return prevRail;
+			}
 
-    // Rail esetén megadjuk az előző pozíciót
-    // ezt összehasonlítva a két szomszéddal, tudjuk mivel kell visszatérni
-    public Rail getNextRail(Rail previous, Train_Element t) {
-        if (previous == prevRail) {
-            // ha aktív a tunnel és a következő rail alatt megy alagút, és még nincs vonat az alagútban
-            if (isActive() && nextRail.isTunnelUnderRail && !Map.getIsTrainInTunnel()) {
-                t.setVisible(false);
-                Map.setIsTrainInTunnel(true);
-                return nextRail;
-            }
-
-            // ellenkező esetben haladunk tovább a felszínen
-            else
-                return nextRail;
-        }
-
-
-        else {
-            if (this.isActive && prevRail.isTunnelUnderRail && !Map.getIsTrainInTunnel()) {
-                t.setVisible(false);
-                Map.setIsTrainInTunnel(true);
-                return prevRail;
-            }
-
-            // ellenkező esetben haladunk tovább a felszínen
-            else
-                return prevRail;
-        }
-    }
-
+			// ellenkező esetben haladunk tovább a felszínen
+			else
+				return prevRail;
+		}
+	}
 
 }
