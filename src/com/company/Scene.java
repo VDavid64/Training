2126198bd37ab0ee8fe03d9863f7225963db339c
vidoree <sivodora@gmail.com>
2017-04-23 21:09:30 +0200

    Add Scene, make JFrame

---
+++ src/com/company/Scene.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JRadioButtonMenuItem;
+
+public class Scene extends JFrame{
+	
+	private final static int WIDTH = 1000, HEIGHT = 600;
+	public Scene(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		setResizable(false);
+		setVisible(true);
+		setTitle("I like trains");
+		JMenuBar menuBar = new JMenuBar();
+		JMenuItem exit = new JRadioButtonMenuItem("Exit");
+		menuBar.add(exit);
+		menuBar.setBackground(java.awt.Color.gray);
+		this.add(menuBar);
+		setJMenuBar(menuBar);
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+}
