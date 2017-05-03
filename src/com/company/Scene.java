711b7ff17c381fda07da2c59cf603aa43221ce3b
vidoree <sivodora@gmail.com>
2017-05-03 21:24:15 +0200

    Start gomb felvétele, menü kialakítása TODO: Start gombra induljon a játék

--- src/com/company/Scene.java
+++ src/com/company/Scene.java
@@ -1,14 +1,18 @@
 package com.company;
 
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 public class Scene extends JFrame{
 	
@@ -23,18 +27,55 @@ public class Scene extends JFrame{
 		setVisible(true);
 		setTitle("I like trains");
 		JMenuBar menuBar = new JMenuBar();
-		JMenuItem exit = new JRadioButtonMenuItem("Exit");
+		JMenu exit = new JMenu("Exit");
+		JMenu start = new JMenu("Start");
 		menuBar.add(exit);
+		menuBar.add(start);
 		menuBar.setBackground(java.awt.Color.gray);
 		this.add(menuBar);
 		setJMenuBar(menuBar);
-		exit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
+		exit.addMenuListener(new MenuListener() {
+			 @Override
+		        public void menuSelected(MenuEvent e) {
 				System.exit(0);
 			}
+
+			@Override
+			public void menuCanceled(MenuEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void menuDeselected(MenuEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		start.addMenuListener(new MenuListener(){
+
+			@Override
+			public void menuCanceled(MenuEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void menuDeselected(MenuEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void menuSelected(MenuEvent e) {
+				// TODO Auto-generated method stub
+			}
+			
 		});
 	}
 
-	public void drawAll() {};
+	public void drawAll(Graphics2D g) {
+		Draw_Rail dr = new Draw_Rail();
+		dr.drawElement(g);
+	};
 }
