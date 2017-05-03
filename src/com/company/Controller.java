4aa01f6a3ef5a12907113a93552d0e13138f07b8
vidoree <sivodora@gmail.com>
2017-05-03 21:06:57 +0200

    MouseListener törlése, Controller a beépítettet használja

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,6 +1,38 @@
 package com.company;
 
-public class Controller {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-    public void mouseClicked() {}
+public class Controller implements MouseListener{
+	int x,y;
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		x = arg0.getX();
+		y = arg0.getY();
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
