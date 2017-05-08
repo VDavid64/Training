eb2ba73e09c911998feef20776e2c5f9a0195686
anadutova <d.anasztazia@gmail.com>
2017-05-08 02:53:21 +0200

    Tunnel kör igazítása

--- src/com/company/Draw_Tunnel.java
+++ src/com/company/Draw_Tunnel.java
@@ -41,71 +41,84 @@ public class Draw_Tunnel implements Drawable {
 	 */
     @Override
     public void drawElement(Graphics2D g) {
-    	 int x = (int)pos.getX();
- 	    int y = (int)(pos.getY());
- 	    g.setColor(java.awt.Color.black);
- 	    if ((myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y+1)
- 	    		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
- 	    		||
- 	    		(myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y+1)
- 	    		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1))
- 	    { //függőleges téglalap
- 	    	g.fillRect(64*x+17, 64*y, 30, 64);
- 	    }
- 	    else if ((myTunnel.nextRail.pos.getX() == x+1 && myTunnel.nextRail.pos.getY() == y)
- 	    		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
- 	    		||
- 	    		(myTunnel.prevRail.pos.getX() == x+1 && myTunnel.prevRail.pos.getY() == y)
- 	    		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1))
- 	    		{
- 	    			// draw GeneralPath (polygon)
- 	    			int x1Points[] = {x*64+17, x*64+17+30, x*64+64, x*64+64, x*64+64-12, x*64+17};
- 	    			int y1Points[] = {y*64+0, y*64+0, y*64+17, y*64+17+30, y*64+17+30, y*64+12};
- 	    			g.fillPolygon(x1Points, y1Points, 6);
- 	    		}
- 	    else if ((myTunnel.nextRail.pos.getX() == x-1 && myTunnel.nextRail.pos.getY() == y)
- 	    		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
- 	    		||
- 	    		(myTunnel.prevRail.pos.getX() == x-1 && myTunnel.prevRail.pos.getY() == y)
- 	    		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1)){
- 	    			int x1Points[] = {x*64+17, x*64+17+30, x*64+17+30, x*64+12, x*64+0, x*64+0};
- 	    			int y1Points[] = {y*64+0, y*64+0, y*64+12, y*64+17+30, y*64+17+30, y*64+12};
- 	    			g.fillPolygon(x1Points, y1Points, 6);
- 	    		}
- 	    else if ((myTunnel.nextRail.pos.getX() == x+1 && myTunnel.nextRail.pos.getY() == y)
-     		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y+1)
-     		||
-     		(myTunnel.prevRail.pos.getX() == x+1 && myTunnel.prevRail.pos.getY() == y)
-     		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y+1)){
- 	    			// draw GeneralPath (polygon)
- 	    			int x1Points[] = {x*64+64-12, x*64+64, x*64+64, x*64+64-17, x*64+17, x*64+17};
- 	    			int y1Points[] = {y*64+12, y*64+12, y*64+17+30, y*64+64, y*64+64, y*64+(64-12)};
- 	    			g.fillPolygon(x1Points, y1Points, 6);
- 	    		}
- 	    		
- 	    else if ((myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1)
- 	    		&& (myTunnel.prevRail.pos.getX() == x-1 && myTunnel.prevRail.pos.getY() == y)
- 	    		||
- 	    		(myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
- 	    		&& (myTunnel.nextRail.pos.getX() == x-1 && myTunnel.nextRail.pos.getY() == y)){
- 	    			// draw GeneralPath (polygon)
- 	    			int x1Points[] = {x*64+0, x*64+12, x*64+17+30, x*64+17+30, x*64+17, x*64+0};
- 	    			int y1Points[] = {y*64+12, y*64+12, y*64+17+30, y*64+64, y*64+64, y*64+(64-12)};
- 	    			g.fillPolygon(x1Points, y1Points, 6);
- 	    }
- 	    
- 	    else {
- 	    	g.fillRect(x*64,y*64+17, 64, 30); //vízszintes téglalap
- 	    }
-    	
-    	
-        if(myTunnel.isActive()) {
-        	g.setColor(java.awt.Color.blue);
-        } else {
-        	g.setColor(java.awt.Color.orange);
-        }
+    	  int x = (int)pos.getX();
+  	    int y = (int)(pos.getY());
+  	    g.setColor(java.awt.Color.black);
+  	    if (myTunnel.nextRail != null && myTunnel.prevRail != null){
+  	    if ((myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y+1)
+  	    		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
+  	    		||
+  	    		(myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y+1)
+  	    		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1))
+  	    { //függőleges téglalap
+  	    	g.fillRect(64*x+17, 64*y, 30, 64);
+  	    	g.setColor(myTunnel.isActive() ? java.awt.Color.blue : java.awt.Color.orange);
+  	    	g.fillOval(x*64+32, y*64+32, 13, 13);
+  	    }
+  	    else if ((myTunnel.nextRail.pos.getX() == x+1 && myTunnel.nextRail.pos.getY() == y)
+  	    		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
+  	    		||
+  	    		(myTunnel.prevRail.pos.getX() == x+1 && myTunnel.prevRail.pos.getY() == y)
+  	    		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1))
+  	    		{
+  	    			// draw GeneralPath (polygon)
+  	    			int x1Points[] = {x*64+17, x*64+17+30, x*64+64, x*64+64, x*64+64-12, x*64+17};
+  	    			int y1Points[] = {y*64+0, y*64+0, y*64+17, y*64+17+30, y*64+17+30, y*64+12};
+  	    			g.setColor(java.awt.Color.black);
+  	    			g.fillPolygon(x1Points, y1Points, 6);
+  	    			g.setColor(myTunnel.isActive() ? java.awt.Color.blue : java.awt.Color.orange);
+  	    			g.fillOval(x*64+32+16, y*64+32-16, 13, 13);
+  	    		}
+  	    //ESZAKNYUGAT
+  	    else if ((myTunnel.nextRail.pos.getX() == x-1 && myTunnel.nextRail.pos.getY() == y)
+  	    		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y-1)
+  	    		||
+  	    		(myTunnel.prevRail.pos.getX() == x-1 && myTunnel.prevRail.pos.getY() == y)
+  	    		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y-1)){
+  	    			int x1Points[] = {x*64+17, x*64+17+30, x*64+17+30, x*64+12, x*64+0, x*64+0};
+  	    			int y1Points[] = {y*64+0, y*64+0, y*64+12, y*64+17+30, y*64+17+30, y*64+17};
+  	    			g.setColor(java.awt.Color.black);
+  	    			g.fillPolygon(x1Points, y1Points, 6);
+  	    			g.setColor(myTunnel.isActive() ? java.awt.Color.blue : java.awt.Color.orange);
+  	    			g.fillOval(x*64+32-16, y*64+32-16, 13, 13);
+  	    		}
+  	    //delkelet
+  	    else if ((myTunnel.nextRail.pos.getX() == x+1 && myTunnel.nextRail.pos.getY() == y)
+      		&& (myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y+1)
+      		||
+      		(myTunnel.prevRail.pos.getX() == x+1 && myTunnel.prevRail.pos.getY() == y)
+      		&& (myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y+1)){
+  	    			// draw GeneralPath (polygon)
+  	    			int x1Points[] = {x*64+64-12, x*64+64, x*64+64, x*64+64-17, x*64+17, x*64+17};
+  	    			int y1Points[] = {y*64+17, y*64+17, y*64+17+30, y*64+64, y*64+64, y*64+(64-12)};
+  	    			g.setColor(java.awt.Color.black);
+  	    			g.fillPolygon(x1Points, y1Points, 6);
+  	    			g.setColor(myTunnel.isActive() ? java.awt.Color.blue : java.awt.Color.orange);
+  	    			g.fillOval(x*64+32+16, y*64+32+16, 13, 13);
+  	    		}
+  	    		
+  	    else if ((myTunnel.nextRail.pos.getX() == x && myTunnel.nextRail.pos.getY() == y+1)
+  	    		&& (myTunnel.prevRail.pos.getX() == x-1 && myTunnel.prevRail.pos.getY() == y)
+  	    		||
+  	    		(myTunnel.prevRail.pos.getX() == x && myTunnel.prevRail.pos.getY() == y+1)
+  	    		&& (myTunnel.nextRail.pos.getX() == x-1 && myTunnel.nextRail.pos.getY() == y)){
+  	    			// draw GeneralPath (polygon)
+  	    			int x1Points[] = {x*64+0, x*64+12, x*64+17+30, x*64+17+30, x*64+17, x*64+0};
+  	    			int y1Points[] = {y*64+17, y*64+17, y*64+(64-12), y*64+64, y*64+64, y*64+(64-17)};
+  	    			g.setColor(java.awt.Color.black);
+  	    			g.fillPolygon(x1Points, y1Points, 6);
+  	    			g.setColor(myTunnel.isActive() ? java.awt.Color.blue : java.awt.Color.orange);
+  	    			g.fillOval(x*64+32-16, y*64+32+16, 13, 13);
+  	    }
+  	    
+  	    else {
+  	    	g.setColor(java.awt.Color.black);
+  	    	g.fillRect(x*64,y*64+17, 64, 30); //vízszintes téglalap
+  	    	g.setColor(myTunnel.isActive() ? java.awt.Color.blue : java.awt.Color.orange);
+  	    	g.fillOval(x*64+32, y*64+32, 13, 13);
+  	    }
+  	}
         
-        g.fillOval(x*64+32, y*64+32, 13, 13);
 
     }
 
